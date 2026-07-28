616ad3c412b6832dac6c991b173c3647cca652a8
Sanyam Jain <sanyamjain850@gmail.com>
2026-07-29 02:00:42 +0530

    Fix pastesSlice import casting in store .js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import pasteReducer from './redux/PasteSlice'
+import pasteReducer from './redux/pasteSlice'
 
 export const store = configureStore({
   reducer: {
